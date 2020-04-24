7078697b5e1cc38436aececc22f9ee6173c76cdc
Aby Kal <abyabrkal@gmail.com>
2020-04-24 21:00:12 +0400

    Add/Get users for model
    Added GetUser and AddUser functions

--- models/user.go
+++ models/user.go
@@ -1,5 +1,6 @@
 package models
 
+// User structure 
 type User struct {
 	ID int
 	FirstName string
@@ -10,3 +11,16 @@ var (
 	users []*User
 	nextID = 1
 )
+
+// GetUsers return the pointer slice of address to user structs
+func GetUsers() []*User {
+	return users
+}
+
+// AddUsers add new user to user slice and return the new user or error
+func AddUsers(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+	return u, nil
+}
